8f30bf25581917d4464f978eadfd13e275bd55b7
HP <HP@DESKTOP-K5K9F57>
2023-04-22 11:25:57 +0530

    My First Commits\

---
+++ src/com/GitHubDemo/GitHub.java
@@ -0,0 +1,12 @@
+package com.GitHubDemo;
+
+public class GitHub {
+public static void main(String[] args) {
+	String hungry="yes";
+	if(hungry=="yes")
+		System.out.println("eat something.....");
+	else {
+		System.out.println("DO work.......");
+	}
+}
+}
